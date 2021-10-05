6a83254b6c0e0cf11b116670b507f6ac60231ec3
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2021-10-04 21:14:53 -0700

    Customize identity (#13)
    * Turned off required email confirmation
    
    * Changed name of database
    
    * Scaffolded ASP.NET Identity into project
    
    * Allow user to register with custom username. Login now uses username appropriately
    
    * Added IdentityOptions for easy customization
    
    * Created default roles
    
    * Added default instructor account
    
    * All new users get student role

--- SpeakingInBitsWeb/Controllers/HomeController.cs
+++ SpeakingInBitsWeb/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SpeakingInBitsWeb.Models;
 using System.Diagnostics;
 
---
+++ SpeakingInBitsWeb/Models/IdentityHelper.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SpeakingInBitsWeb.Models
+{
+#nullable disable
+    public static class IdentityHelper
+    {
+        public const string Instructor = "Instructor";
+        public const string Student = "Student";
+
+        public static async Task CreateRoles(IServiceProvider provider, params string[] roles)
+        {
+            RoleManager<IdentityRole> roleManager = provider.GetService<RoleManager<IdentityRole>>();
+
+            foreach(string role in roles)
+            {
+                bool doesRoleExist = await roleManager.RoleExistsAsync(role);
+                if (!doesRoleExist)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+
+        public static async Task CreateDefaultUser(IServiceProvider provider, string role)
+        {
+            var userManager = provider.GetService<UserManager<IdentityUser>>();
+
+            // If no users are present, make the default user
+            int numUsers = (await userManager.GetUsersInRoleAsync(role)).Count;
+            if(numUsers == 0) // If no users are in the specified role
+            {
+                var defaultUser = new IdentityUser()
+                {
+                    Email = "instructor@speakinginbits.com",
+                    UserName = "Admin"
+                };
+
+                await userManager.CreateAsync(defaultUser, "Programming01#");
+
+                await userManager.AddToRoleAsync(defaultUser, role);
+            }
+        }
+    }
+}
