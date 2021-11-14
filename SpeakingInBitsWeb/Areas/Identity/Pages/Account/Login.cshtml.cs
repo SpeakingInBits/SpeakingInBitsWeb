a22d015836234251ba3a7cfc4ae3178df69463f7
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2021-11-14 14:12:16 -0800

    Set up email

--- SpeakingInBitsWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SpeakingInBitsWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -18,6 +18,7 @@ namespace SpeakingInBitsWeb.Areas.Identity.Pages.Account
     public class LoginModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<LoginModel> _logger;
 
         public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger)
@@ -63,7 +64,8 @@ namespace SpeakingInBitsWeb.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            public string Username { get; set; }
+            [Display(Name ="Username or Email")]
+            public string UsernameOrEmail { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -108,7 +110,13 @@ namespace SpeakingInBitsWeb.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                IdentityUser user = await _userManager.FindByEmailAsync(Input.UsernameOrEmail);
+                if (user == null)
+                {
+                    user = await _userManager.FindByNameAsync(Input.UsernameOrEmail);
+                }
+
+                var result = await _signInManager.PasswordSignInAsync(user, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
