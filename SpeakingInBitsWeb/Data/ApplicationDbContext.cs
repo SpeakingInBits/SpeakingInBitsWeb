b3a92aaf48f7f570bd01fe7019966ce4ceea10df
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2025-09-21 17:07:51 -0700

    Add project files.

---
+++ SpeakingInBitsWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SpeakingInBitsWeb.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
