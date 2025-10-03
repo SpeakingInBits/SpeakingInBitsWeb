80a59a4c4c6d51a20fb3862cd003a4eba1cd4521
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2025-10-03 11:08:06 -0700

    Customize ApplicationUser with FirstName and LastName.
    Introduced `FirstName` and `LastName` properties to the `ApplicationUser` model.
    Added constraints to enforce a maximum length of 50 characters and marked these fields as required.
    
    Created migrations `20251003175158_CustomizeUser` and `20251003175653_AddedNameLength` to update the database schema:
    - Added `FirstName` and `LastName` columns to the `AspNetUsers` table.
    - Updated column definitions to enforce length constraints.
    
    Updated `ApplicationDbContextModelSnapshot` and related designer files to reflect these changes.
    Adopted modern EF Core conventions and annotations for improved maintainability.

--- SpeakingInBitsWeb/Data/ApplicationDbContext.cs
+++ SpeakingInBitsWeb/Data/ApplicationDbContext.cs
@@ -6,5 +6,20 @@ namespace SpeakingInBitsWeb.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>(entity =>
+            {
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(50)
+                    .IsRequired();
+            });
+        }
     }
 }
